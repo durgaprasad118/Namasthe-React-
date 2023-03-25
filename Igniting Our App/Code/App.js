ee6fe1f393f8eff62b6300dee8f87ce9d8db7ee9
Achana Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2023-03-25 13:50:28 +0530

    chapter2

---
+++ Igniting Our App/Code/App.js
@@ -0,0 +1,27 @@
+
+// Manipulating DOM using React
+const element = React.createElement(
+  "h1",
+  { id: "heading1" },
+  "Hello World form React"
+);
+// creating root using ReactDOM (from CDN)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
+
+// Creating a nested React Elements
+
+const elements = React.createElement("div", { class: "innerDIv" }, [
+  React.createElement(
+    "p",
+    { id: "para" },
+    "I am a paragraph form nested React elements"
+  ),
+  React.createElement(
+    "h3",
+    { id: "h3" },
+    "I am a heading3 form nested React elements"
+  ),
+]);
+
+root.render(elements);
---
+++ Igniting Our App/Code/README.md
@@ -0,0 +1,15 @@
+
+# _Chapter 02 - Igniting our App_
+
+
+## Coding Assignment:
+- In your `existing project`
+    - initialize `npm` into your repo
+    - install `react` and `react-dom`
+    - `remove CDN links` of `react`
+    - `install parcel`
+    - `ignite your app` with `parcel`
+    - `add scripts` for `“start”` and `“build”` with `parcel commands`
+    - add `.gitignore` file
+    - add `browserlists`
+    - build a production version of your code using `parcel build`
